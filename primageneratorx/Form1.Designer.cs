3c42508a2ee25a69fa21861fa89a2e98671db9a7
jake hartley <puffleboy101@hotmail.co.uk>
2019-08-10 14:37:36 +0100

    -updated to be the same method as dicpicbot

--- primageneratorx/Form1.Designer.cs
+++ primageneratorx/Form1.Designer.cs
@@ -28,8 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -39,7 +45,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Get banned";
+            this.button1.Text = "Blacklist me";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -47,24 +53,76 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(304, 322);
+            this.pictureBox1.Size = new System.Drawing.Size(634, 632);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(93, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(42, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1x1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 679);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(634, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(570, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(420, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(63, 30);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "show \r\nprocess";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(489, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "info";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(329, 376);
+            this.ClientSize = new System.Drawing.Size(657, 709);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "illegal maker";
+            this.Text = "Prim-o-tron";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +130,11 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- primageneratorx/Form1.cs
+++ primageneratorx/Form1.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,45 +20,148 @@ namespace primageneratorx
             InitializeComponent();
         }
 
-        public Bitmap primabitmap;
-        public Bitmap drawn = new Bitmap(100, 100);
-        public const string prima = "iVBORw0KGgoAAAANSUhEUgAAAGQAAABkCAYAAABw4pVUAAAACXBIWXMAAAAAAAAAAQCEeRdzAAAHJUlEQVR4nO2cTY4kRQyF5wZchCWnYIHEEs7AhgULzsLFmFMAG5DYNMoRHnlePzvsSEc4uzNDsqayKisr6n3pv6jo+fDy8vLhsetY+wQee4Bc2ton8NgDJG0/fv/bi9gD5AJ2gPjrn38/2Woo7V/2Ssa8QMPYAaVdhNX2zdc/vYhFxP/zj9+/eJ4BWQmlXbAdQL779odPpsFo8UVcBCKvMSCroLQLthOIBiNiHwA0GHa8E0q7YF1A/v741WdBtQkQAaEf74DSLli1YW5gQBAKiusBwnOP61RCaRdwBRDMDVkoDA6GMoEj16mC0i7gSigioAUkAoXBERBix/uroLSLVyE+dtIYag6rgKLByHE1lHZBK4BgnGfx34OSATICw3qeWwOxqiQPygiIbi4t0+fKNW8JJAIFX0coIuoBhYHxwh1eAxvRB0jQLE9hYLxzI6BuB+QMlAiYUUFQCaVdyE4gUTBR0SugtAu5EwqWyGgzniBeNIJySyAeFC1WtKIaAREQ+vFZKO0C7oKCQlkVlVfqRoCchdIuXiUEz6zKSsBo033FqPTV74uEuFsBmS13UTD0kEw/chZKu5BdUDyxPEgsmWehXAYICyWrPiMDhuUKrJ4YEMwdFVC2QmBj1WYBK4/MdvBngGTC1nIQkbEaykyjiCGJ5ZBI2PJeZ1CWwTgGq2C0rQQSBfHLz78OPWWmymJAIouPS2Do8tGzVVA8GMfnCgiEgcd6rsfxKAxlw9dyIKNGik1oBRRrWeQwAWGZ502zQLYn9SiAnVBwflp0gSTH+vUzfQwDYlVt1lzbQFhAqqEwGEzkqJeMwGDHr0HIc95820DsgqLF9hYes0CyDaacO5rv1BesAuEBqYCi89ko0WdyCRYCIzDLfqBaBaMaCv62MQJyBooAsTr/7NzbYFi/OZyBgqsCEe9gQOTx6HwEctiWjXIrYFSGL1wVkMf632hOyFRd+vxtQHbCiEDRX9hanpkFomEwKFp4nTu2AamupKLXs4AQMO7r2YVFnT9Y5cWAoBe9CSAz14lA8UDgcx4IbBQtKLjswkLWWSiXhJH1lLM5hMFgorPEvwXIFWBEoVigqoAwKJbnXBrI6k5eD1meQG+pAjKypUCuBgOhoK0OWZ5nXB5I9RoXXhetquwd5RC96yQCpKxTnxVzNQh8Plr26uMZIAJDPpdBYTCYd2mNPVghINYdmrUzYW+0WUKDwHNGC4tWotbz1r9leEBGYY/pSYFoAZiYGbdjro1ffgSfeQNr+DwIHhBZgPQgYP5iXoK9ySgXsZtN6/uFiLNxLwIEO1oEhBPN7FqxtoPqXDPqzCMJXUKYLoOz1Ri7KSmQaotCYXAsENbulVF4PAOEeTD2J1koLCIsB+JB8cCosvEViJncJDAQSkZEBkREzEDxQuIWIDNQGIzZwkBD0I9n7mqd+L3KiRUIzDu2h6woFA1Hw2B/35cBgh4x24173jH6zgwQg6LftwWInqD1xREGekQWhrW7ZDQPz0P08ez3H1Wv24B4YASG91evkVyR+e3DEgjnx5K5eFzVzphWIAjm+FKYtGdgRCBkKi0vJ+gwWA2mDQiCsUT3YGioGe+YrbasguTdABlVTpa3iKEo7PEKGCvDVjsQLxSxigTvTslBleEqC+VdAInCiFRnlqdYfY4GMtNt6/e9i5CVgeGJgmHKClvWeTNAsPzVOexdA4kIZW318Y6lxNZN3mxP8n8f8XlUQLkUDG9JIgJHC86OcVlmplEUCGqupVAuBcN6T0Qo9iuhHB/XB8FegckCscabB2ItIcwscbCNDvqzLCBRKCMYFVDaYXjnR5OrBSMDJLL4+G6BWF7B3uOtM2W8YxS2IluBMmMWynYgMyul+hpW1YNbf47BcpUFRJ6/FZAzMPQ12PI1AjnEZ/+fFQj2SsQKGPAZ1wSy2lYAsUrb6Lg9EAxX1v/4psR6JSB4n3luZDxAIJl7QJjQWkBvn1dmZKG0C1llDIhnTGQUzyqlM+MBQkYFEHZ+ZNwaiAgAP62OxHLFOwvltkCYeCKIZxHhZqHcOqkz4TJjJB5bag964ANkZkSAsL5kxuM8axex0lZ21MZaHL3Wm1hcXG2wVpYaEe9gu2C8pZxbA8HtRBkoURje1qSzi6bvEoi1bcgDEQ1VEc+ogtIu5iognngZ4XA7UnQ/wAMEhKkMKwjF8pAKL2kXswpIJKycBRK1M1DaxawC4iXcCqFG4dBYJ7snEITimZyb9ZpRtaWPZdy6U4+GFetOz3zGMZzrfDGyobJdyCsAiUKJ/N6iocyUxO1C7oYiQjFAEdE8oFZeyUBpF7HSvDsYB+svZoGIV7BGMduntIu4Aog3rO76bILXn215x+2AIBS9fMJAnO1N9PtxGX72c9oFXA2EhY6qrjoKLfOedgFXQdF3qQVlNZAZa5/AarOSd9Vi4ANkAgjr0rvnZdl/UnLOB7qS7KAAAAAASUVORK5CYII=";
-        public Color baseprimary = Color.FromArgb(117, 113, 121);
-        public Color basesecondary = Color.FromArgb(240, 239, 216);
-        public Color basehighlight = Color.FromArgb(246, 217, 15);
-
-        public Random rnd = new Random();
-
-        Color gencolour1()
-        {
-            var fstclr = String.Format("#{0:X6}", rnd.Next(0x1000000));
-            Color firstclr = System.Drawing.ColorTranslator.FromHtml(fstclr);
-            return firstclr;
-        }
-
-        Color gencolour2()
-        {
-            var fstclr = String.Format("#{0:X6}", rnd.Next(0x1000000));
-            Color firstclr = System.Drawing.ColorTranslator.FromHtml(fstclr);
-            return firstclr;
-        }
-
-        Color gencolour3()
-        {
-            var fstclr = String.Format("#{0:X6}", rnd.Next(0x1000000));
-            Color firstclr = System.Drawing.ColorTranslator.FromHtml(fstclr);
-            return firstclr;
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
-            Color PRIMARY = gencolour1();
-            Color SECONDARY = gencolour2();
-            Color HIGHLIGHT = gencolour3();
+            try
+            {
+                Thread a = new Thread(() => doPrima(textBox1.Text));
+                a.IsBackground = true;
+                a.Start();
+            }
+            catch
+            {
+                doPrima("1x1");
+            }
+        }
+
+        public void doPrima(string dim)
+        {
+            String[] dimr = dim.Split('x');
+            int dimx = Int32.Parse(dimr[0]);
+            int dimy = Int32.Parse(dimr[1]);
+            int numOfPrimas = dimx * dimy;
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                progressBar1.Maximum = numOfPrimas;
+            }));
+
+            string dppath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/dicpicbot_data/";
+            string tag = "-c";
+            Size psize = new Size(342, 341);
+            Size mSize = getPrimSize(dim);
+
+            Size newPrimaSize = getPrimSize(dim);
+            List<Point> positions = getAllPositions(dim);
+            Bitmap BaseBmp = new Bitmap(newPrimaSize.Width, newPrimaSize.Height);
+            Graphics gr = Graphics.FromImage(BaseBmp);
+
+            int count = 0;
+
+            foreach (Point p in positions)
+            {
+                Bitmap prima = GetNewPrima(tag);
+                gr.DrawImage(prima, p);
+                count++;
+                this.Invoke(new MethodInvoker(delegate ()
+                {
+                    progressBar1.Value = count;
+                }));
+
+                if (checkBox1.Checked){
+                    this.Invoke(new MethodInvoker(delegate ()
+                    {
+                        pictureBox1.Image = BaseBmp;
+                    }));
+                }
+
+            }
+
+            this.Invoke(new MethodInvoker(delegate ()
+            {
+                pictureBox1.Image = BaseBmp;
+            }));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            doPrima("1x1");
+        }
+
+        public List<Point> getAllPositions(string dim)
+        {
+            List<Point> points = new List<Point> { };
+
+            string[] dimxy = dim.Split('x');
+            int x = Int32.Parse(dimxy[0]);
+            int y = Int32.Parse(dimxy[1]);
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int o = 0; o < y; o++)
+                {
+                    points.Add(new Point(342 * i, 341 * o));
+
+                }
+            }
+
+            return points;
+        }
+
+        public Size getPrimSize(string dim)
+        {
+            string[] dimxy = dim.Split('x');
+            int basex = 342;
+            int basey = 341;
+            int x = Int32.Parse(dimxy[0]);
+            int y = Int32.Parse(dimxy[1]);
+            return new Size(basex * x, basey * y);
+        }
+
+        public Bitmap GetNewPrima(string tag)
+        {
+            Bitmap drawn = new Bitmap(342, 341);
+            Color baseprimary = Color.FromArgb(107, 112, 123);
+            Color basesecondary = Color.FromArgb(217, 224, 226);
+            Color basehighlight = Color.FromArgb(250, 178, 60);
+            Color basefeet = Color.FromArgb(255, 242, 0);
+            Color baselegs = Color.FromArgb(34, 177, 76);
+            Color baseface = Color.FromArgb(78, 82, 93);
+            Color baseweird = Color.FromArgb(78, 82, 93);
+
+            Color normallegs = Color.FromArgb(240, 247, 249);
+            Color normalface = Color.FromArgb(78, 82, 93);
+
+            Random rnd = new Random();
+            Bitmap primabitmap;
+            const string prima = "iVBORw0KGgoAAAANSUhEUgAAAVYAAAFBCAMAAAAboneSAAAABGdBTUEAALGPC/xhBQAAAwBQTFRFAAAAOzJ0TlJdYGVva3B7IrFM+rI8//IAqNrC2eDi8Pf58fj5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAxapfrAAAAQB0Uk5T////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////AFP3ByUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuMjHxIGmVAAALWUlEQVR4Xu3b67KdNhqE4ck4PiS5/+v1NOrmGyFgHbZpYDv9/FglhATidZVrXDX5z88wSFaLZLVIVotktUhWi2S1SFaLZLVIVotktUhWi2S1SFaLZLVIVotktUhWi0+f9Y+Zru8hWS2S1eJ3yPp3c6uyyWqRrBafLCva9fkwZlPqb10rWS2S1eJeWVu0ia63On758qVmMFDRWb/4QslqkawWt8v63wYDBsIvOw6XHINydnj3WslqkawWN81KrdsEKSvuMMavcnYwqSdeJFktktXi7lm/ffv2zz//YFCYFWqMAVPSHcomq0WyWlyflS1qrKIzzKAsS1FbLpUVcKu28BK/fOz5ktUiWS1ukfXPP/9sKf748eMHflmnYGYoS23HhGUxw/VYzF1co9ecK1ktktXi+qyAj0fZiss6PUxuliXuwkCrb1A2WS2S1eKarK2D8JJZCZes08Pkg7IwPat1BO25rmyyWiSrxWVZlXD++xR03eBSYWaYeZyV2pMWcTmjF58lWS2S1eL6rGWYxCW7EC43s2J+Ty3gdr37FMlqkawW12QFfKf6daYY3TzGbAq8hbJD3H7NgFugLvVuv2S1SFaLe2Vdqyg0RVrG3VygixXc0uvNktUiWS0uywr4SMV7qIXajTvcos1Jwi293ilZLZLV4hNkJSwGtWk4A7pe2buFeZ3AJlktktXiyqyAL1S2panWDrXpbE7S3nq93iZZLZLV4uKsgI9Uy1m1wP9+4j+lerhLXAP9eG3zLib1eo9ktUhWi+uzAj5SRZs+xPQPqYdxSasbbgFdX1E2WS2S1eLKrC3IhqEsqNMMd1W3GRYTV+piqyxmdA6DZLVIVouLs05/le7YjAWYZ0p6XFajZr0GMzrK0ZLVIlktrswK+DBV3IK7oAazYQaXL2aF9V6d42jJapGsFhdnBXwbKOQWLuihCJKxGi73sm4aVuJS5zhUsibrDDmS9SX4vJ6KbsFd5NjLysnXYaNOcKhkTdYGd5EjWV+FLwTFewjLKisMf7f2twab89irExwnWZN1hmV9u2Td9WLQUh2HpsSyoOtZzWBL7cJAhzhOsiZrp6Ik6y58mGp1Nid7U5gGYwZaW5ddwxN0juMka7IuTUUbjFVo5d+edY3tXqRCK+useLJGDS51iEMla7LuU6SVf3XWXh90qjtf9vObsECpOkNWrsEk53Gptx4tWS2S1eJeWft2Q8fhcg/D9RgRcIsDTuJSbzVIVotktbhp1nXE9cwerAQ2Lf0MF+iVHslqkawWt8tKKtTZnHyAz1HIGSf1MqdktUhWi3tlJXy52iztzW/i4qliRy/wS1aLZLW4XVZ8PLus1a231mCgR58oWS2S1eJ5VhyLdG2GFzHHJt59aw0GevSJktUiWS2eZMWZ/vrrrzPPVzk24TA80rCsv8SY6lKPPlGyWiSrxRtZ4YQjVo5NOAwNy/rL88+8lqwWyWrxXlZwn7IP1MM8tKTSrxzG67t6+lmS1SJZLV7KCjpgYz1ln6NXJ+lxMX456JfVDGCsp58lWS2S1eIjWQHzprPisXrHEuZ5kgHnp9M0nASMtTNZAY/VO5Ywz5MMOD+dpuEkYKydyQp4rN6xhHmeZMD56TQNJwFj7bxzVtAZO9N3vHNirgddb8FdPX0J8zrHa/rnPH6jQ7JaJKvFL2UlrHlMz+qS9ZODWjPAvM6xhHnS9Qwz2pmsUGsGmNc5ljBPup5hRjuTFWrNAPM6xxLmSdczzGjnDbNCf2Id803TR3dqkg/noOCSCwaY1yE6mPy7Wd/tn4Mx6R1+yWqRrBZnZB3ggTXgGL962TtZMYOg/E+thrL9Q4Yx1IuIl8dK1mRdwsxvkhV0wIPg4TWo13Gmh0nSIVo44n9rgbIY694y61rbN23kgPj2oySr8O1HSVbh24/yPCvgrTpvo9MdBA+vAd/FS8JlP8NLYE3gePjrFQNt2MFdw3P4sYdIVsGYH3uIZBWM+bGH+EhW0OmOhheBLl7A9VRZ8avb+9qO/0vWBa6nZN3VvvFfkxXwVgYtOuDR8CKNXjD9pTirE774BCzTzrYX9KlHSNYJxqBPPUKyTjAGfeoRXs0KeDHP3dMZD4JX1O+LFGbOyjpPn4AFpM1tOz/zEMkqGPMzD5GsgjE/8xBvZAW8mzV7Oukvw8OHwSuwmHASDuqoPa2eYaZqcoxfbjxEsk6SdYTFhJNwUEftafUMMzfKCng9a67pyB+Cx2r0TlamAZ7qQRrcKtyLQe0FrTtIsibrCrsAT/WgDm4V7sWg9oLWHeTtrMBzMOUaD/0WPlAXDWdA1/u4jK/GQEd8iFt6unGcZJ3oxnGSdaIbx/lIVuKB+D1PqUGH20lTW7RihXfrL8d6C+/qiNdJVotktfh4VuJnAD9sExMMsEWjF+hBHb6UNDW/SLOXxk1Wi2S1+NWsRZ+y6stPLf0MFmv0DB/1Cm1oD9fJrpCsFslqcVjW0tpO9KFL+uj22VBjDjZp52uwnk/WaS6SrBbJanFkVn5PT986YybgXV08o80vwxY8XGe6SLJaJKvFwVn1Zc/0Tff61jzXc+NTXMntOtYVktUiWS2OzAqvfH/1wqAfrwdQWzh4oF9cdKzTJatFslocnBX0QfshdLvpCw5j/H79+hW/3MK9D0zb5sXYSBjrWOdKVotktTg+a2mfOeFnr+FWn68fc0C8BG3bgQW1mBsJlzrQiZLVIlktjFkBn6SP3tIKKEE/hn5MmNG2HesthHmd5kTJapGsFq6s+BjQF+/Dmvp4/PL/UwU103vwwPXiHu7qWGdJVotktTBm1Rcv9fPVggMVndV82XsmDCsHuKtjnSVZLZLVwpUV8DEDNZhhpj4bv8o5q/myfgINyzZhjY51imS1SFYLY9beUASXxA/Gr1p26lYPM3pEZ71sDWt0lFMkq0WyWpyXtVeT/GD8ouO3zutZ12v2YCXfe4JktUhWi5OybmIR/n4gK8brBQ9gsV7sl6wWyWrx+bKWmuHgKazUi/2S1SJZLW6UdVC3epWmv7Vetqe2uyWrRbJafNasUHfXy/b0262S1SJZLa7Pyq/Fr3I2NTnou/QLNhev9dutktUiWS1ulBWqKWyWGrr0azbXD4btPslqkawWV2aFajFE2Wy0GaVfubmrbG43SVaLZLW4S1bAuGhqCfPattSvb7vf2+6QrBbJanGjrE/tdZlCNlq39SeEgVafIlktktXi02R90AW3+M8zDEAbZpzU0rMkq0WyWlyZFV/LFgrw0F6aekgLO8G4p3XnSlaLZLW4LCu/uUIo3g4s0LalegL0Y9rbdYJktUhWiyuz8ssrAahih/Pas8K7fALwsnBGS8+VrBbJanGXrDTFWNLqHVjw/ft3rtQjOpzX0nMlq0WyWlyctT6e44IZrXsGK6vsAPO8paUnSlaLZLW4Miuo4hy3p3XPYCXzPfD6046SrBbJanFlVn6wui69FWIoi0vSdYNLrT5Fslokq8VlWYFf3gps4ILCLZtwl+0AY/3JtD8bzTaPH3KsZLVIVovrs+7pA8HjKPWo9S7OEy5Be5yS1SJZLT5NVnhQhI/SuqV6CwbAGW2zSVaLZLW4MivgC/nNm1iBIWqgnSu1pnAGvzRMaptHslokq8Wts0JVqIF2rnBBwSW3E2fqFmibR7JaJKvFfbPy44EhuJiDPbjLcMDF9aiarFva45GsFslqcXFWwBfWx3PMAVQC0OpntLr7YygfeNqHJatFslrcKyu/HL/DQEt/DZ5z1KOeSlaLZLW4Pivwg6E69gP8at3nkawWyWpxi6ylOvYD0O3PI1ktktXidlmpxpz/dJLVIlkt7pX1t5GsFslqkawWyWqRrBbJapGsFslqkawWyWqRrBbJapGsFslq8PPn/wBr0tiX7+5SBwAAAABJRU5ErkJggg==";
+            byte[] byteBuffer = Convert.FromBase64String(prima);
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+            memoryStream.Position = 0;
+            primabitmap = (Bitmap)Bitmap.FromStream(memoryStream);
+            memoryStream.Close();
+            memoryStream = null;
+            byteBuffer = null;
+
+            Color PRIMARY = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color SECONDARY = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color HIGHLIGHT = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color FEET = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color LEGS = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color FACE = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
+            Color WEIRD = Color.FromArgb(rnd.Next(0, 255), rnd.Next(0, 255), rnd.Next(0, 255));
 
             SolidBrush pr = new SolidBrush(PRIMARY);
             SolidBrush se = new SolidBrush(SECONDARY);
             SolidBrush hl = new SolidBrush(HIGHLIGHT);
+            SolidBrush fe = new SolidBrush(FEET);
+            SolidBrush le = new SolidBrush(LEGS);
+            SolidBrush fa = new SolidBrush(FACE);
+            SolidBrush we = new SolidBrush(WEIRD);
+
+            SolidBrush len = new SolidBrush(normallegs);
+            SolidBrush fan = new SolidBrush(normalface);
+            SolidBrush wen = new SolidBrush(normallegs);
 
             Graphics g = Graphics.FromImage(primabitmap);
             Graphics drawing = Graphics.FromImage(drawn);
@@ -82,6 +187,50 @@ namespace primageneratorx
                     {
                         drawing.FillRectangle(hl, i, o, 1, 1);
                     }
+                    else if (p == basefeet)
+                    {
+                        if (tag == "-c")
+                        {
+                            drawing.FillRectangle(fe, i, o, 1, 1);
+                        }
+                        else
+                        {
+                            drawing.FillRectangle(len, i, o, 1, 1);
+                        }
+                    }
+                    else if (p == baselegs)
+                    {
+                        if (tag == "-c")
+                        {
+                            drawing.FillRectangle(le, i, o, 1, 1);
+                        }
+                        else
+                        {
+                            drawing.FillRectangle(len, i, o, 1, 1);
+                        }
+                    }
+                    else if (p == baseface)
+                    {
+                        if (tag == "-c")
+                        {
+                            drawing.FillRectangle(fa, i, o, 1, 1);
+                        }
+                        else
+                        {
+                            drawing.FillRectangle(fan, i, o, 1, 1);
+                        }
+                    }
+                    else if (p == baseweird)
+                    {
+                        if (tag == "-c")
+                        {
+                            drawing.FillRectangle(we, i, o, 1, 1);
+                        }
+                        else
+                        {
+                            drawing.FillRectangle(wen, i, o, 1, 1);
+                        }
+                    }
                     else
                     {
                         drawing.FillRectangle(old, i, o, 1, 1);
@@ -90,28 +239,29 @@ namespace primageneratorx
                 }
             }
 
-            pictureBox1.Image = drawn;
-
+            return drawn;
         }
 
-        public static Bitmap Base64StringToBitmap(string base64String)
+        private void button2_Click(object sender, EventArgs e)
         {
-            Bitmap bmpReturn = null;
-            byte[] byteBuffer = Convert.FromBase64String(base64String);
-            MemoryStream memoryStream = new MemoryStream(byteBuffer);
-            memoryStream.Position = 0;
-            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
-            memoryStream.Close();
-            memoryStream = null;
-            byteBuffer = null;
+            using (SaveFileDialog dlg = new SaveFileDialog())
+            {
+                dlg.Title = "Save";
+                dlg.Filter = "Png files | .png";
 
-            return bmpReturn;
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    Bitmap b = (Bitmap)pictureBox1.Image;
+                    b.Save(dlg.FileName, ImageFormat.Png);
+
+                }
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            primabitmap = Base64StringToBitmap(prima);
-            pictureBox1.Image = primabitmap;
+            Form info = new info();
+            info.Show();
         }
     }
 }
