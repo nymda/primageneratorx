8640032bdf3791971f94e776c5c7be1eb6f8ecc7
jake hartley <puffleboy101@hotmail.co.uk>
2019-08-17 22:11:25 +0100

    -general betterness or something

--- primageneratorx/Form1.Designer.cs
+++ primageneratorx/Form1.Designer.cs
@@ -39,6 +39,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -63,7 +64,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(136, 14);
+            this.textBox1.Location = new System.Drawing.Point(115, 14);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(42, 20);
             this.textBox1.TabIndex = 2;
@@ -108,7 +109,7 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(231, 14);
+            this.textBox2.Location = new System.Drawing.Point(186, 14);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(42, 20);
             this.textBox2.TabIndex = 7;
@@ -119,24 +120,37 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(92, 17);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.Size = new System.Drawing.Size(17, 13);
             this.label1.TabIndex = 8;
-            this.label1.Text = "Width:";
+            this.label1.Text = "X:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(184, 17);
+            this.label2.Location = new System.Drawing.Point(163, 17);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.Size = new System.Drawing.Size(17, 13);
             this.label2.TabIndex = 9;
-            this.label2.Text = "Height:";
+            this.label2.Text = "Y:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(234, 14);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(54, 17);
+            this.checkBox2.TabIndex = 10;
+            this.checkBox2.Text = "Name";
+            this.checkBox2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 687);
+            this.Controls.Add(this.checkBox2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
@@ -170,6 +184,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox2;
     }
 }
 
--- primageneratorx/Form1.cs
+++ primageneratorx/Form1.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -22,7 +23,8 @@ namespace primageneratorx
 
         public const int _width = 260;
         public const int _height = 305;
-        public const string _base = "iVBORw0KGgoAAAANSUhEUgAAAQQAAAExCAMAAACtYSwmAAAABGdBTUEAALGPC/xhBQAAAwBQTFRFAAAAOzJ0TlJdYGVva3B7IrFM+rI8//IAqNrC2eDi8Pf58fj5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAxapfrAAAAQB0Uk5T////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////AFP3ByUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuMjHxIGmVAAAKEElEQVR4Xu2a7Y7jOBIEb293Z7/e/3nnQsx0HU1RsmS75V4g44dQLBbJqgCmp4GZ//wMPyMBIgEiASIBIgEiASIBIgEiAf4tEn654fVbiQSIBPgXSfi78RUeIgEiAb6nBCbthyWWAdFvvYVIgEiAj0hoIy54PZv6119/rQyB57/RF79OJEAkwKck/LdBoHH4auphqRg8fId230IkQCTAZyWINuUCg5eKIebr4TtI+sbXiASIBPgmEn78+PHPP/8QFJIAFRNocPFGD5EAkQCXSVDnFXv+G2TwoLlEKzclAdiqI1ry1bVPEwkQCXClhN9++601/suff/7JV7MUZAYPop1YkAcyqqdYp1TjZ54iEiAS4DIJQKt4KBWapYfk1IPQKQJXv89DJEAkwJdKaF0bLSVBsNQsPSR3PMByV5safOZlD5EAkQBfLcED334OgNcNlh7jBpl9CaLddKdCGT98kkiASIDLJBRDkqWmECynEshvUQU67rfPEAkQCfClEoCuPG3H0nqXJ5YB0BYeBhV9zYCOQC399mEiASIBPiJhTY0glpHuVUwLvFjBlp8/RiRAJMBXSwBa8qi7tLE2VQxbYpoUbPn5A0QCRAJ8HwmCYvAkDWXA6xVbW+TdwSMiASIBLpAA9OMh71lm28CTdEyTYqvezz8iEiAS4BoJQEue/EZ1zt+A+rWwh12hGujjNdNdkn5+l0iASIDLJAAtef5G3/byS+GuCuHqho6A1y94iASIBLhAQmt/wuABPNUNdu2iMRQLVXox80DGfWwTCRAJcI2E5UfABtPRgLwGF/seHDXWNWTcygaRAJEAF0gA2vDMM9gFd3xjyLA8KAHWZ93HBpEAkQDXSAA6AY89QwU99M+AmpHlloQpQyVL9zEjEiAS4DIJgmZ6PP8Mdml+S4KSx+GgO5gRCRAJcLEEoB/wqLtQVhJg+JnQbw1M85x1BysiASIBrv+Z4AmPUVMPBoQ8gNc3KsOROkXgJlZEAkQCXCmBNjxbxzTZs4zRINY4a9Ye1nCD+1gRCRAJcLGENZr0IJ5nxVoCNztqsHQTMyIBIgGulNDTj7+4uC37/BQKPFjHIEE1JJVn6Vc3iASIBPiIhH7SYephuYXG7NHIwJYCJVn61W0iASIBPithPfI6swWVIANFn1GBn9wlEiAS4FMShOfpmCZ30D0e+4aSfuwAkQCRAB+RIOjTk9yzlZ+i4mXmDj9wmEiASIBPSaBVTbGmtk7VEPjq80QCRAKUBC4RXn8xPKTmp2j3VA2Brz5PJEAkgCRww19//fX6bcep5qfQjFoayvolsailrz5PJEAkwFoCvHLhQar5KTQjhrJ++caeIwEiAaYS4JU7j9CP00MemgDTVw7xete3nyQSIBKglwC+rvH0nUfom++pTnpUzFdBX1YZIPbtJ4kEiATYkQDkn755H671G/eQVycDyi/dNJQEYp+MhAaxbz9JJMAgAXxjx/LqmftVD17PYNe330PefRyjv2f/xR0iASIBjkgQ1OzTblogriNOraiaAfLu4x7ywusbZHwyEhrEvv0kkQCSAP39vvQkS4sdldTlCgqWKhgg7yY6SP7dWO/29xALv3GYSIBIgDdKGODCChTz9WNnJJBhfP3XxMFDf8kQQz0ktJwSCRAJMJUAvu5NcHkF9ZwyPSSFm2hjCv2PLDwQe+9ewpp2bjmoQOj1gUiASICSANT49obvehNcXoHe0lKw7DNagmYHxcOPBQIf2ECnhns0bE8kQCTAjgTwXe+Gh8CLA6helAS+3t6mnfg/kQCRAEckADUav/B174aHHB1g+cN8ozo8eANlPtnOgkftiASIBBgkAGV6pcc3vgmeqO9BPMZNgmZ5eAMFwofbcY3ZEwkQCbCWAFRq9h7f+zJcPgRHoFjQiYJqtcfVN8jU7Ir56mBPJEAkwFQCUKzZ1/iBp+BaR2ckaBBQV9NBBFuFzhLUWXDdPZEAkQBbEkCnNPgaPXEKXehFQxnwehuV6WkCt7iLjvR4Y0UkQCTAjgSh43r9Ie64Q8eFUzNcsUK79Ye6XtGuW3yZSIBIgIcShB4FtTFFDQ9wxNEBfFGHHhVO3R5y9h0qIgEiAQ5KKPzwyoYaK/oMxY4eoauO4APtcnf2ApEAkQBnJRTNxILbuscttiahYgVTfPIY1Otmd/MakQCRAE9I0Os97uyGhgLtevEIHz4MR7jcPb1GJEAkwHMS3McjegNbNiqveh18iCp13G29QCRAJMATEuBItzUdQR+vA6gjCnboiwu39SyRAJEAz0kAP7/dtrcb/bxDzPf333/nqyM6u8Ny7FbMQUHstp4iEiAS4GkJRWtqQU2uYasfto8VCC3BxzagoIp1ULB0Q+eJBIgEeF0C0IBbnNH6dcN9DH0syPjYBusjgry7OU8kQCTAixJ4GtzfNtRUq3z1b4tQmZ6dC9fFPey6rZNEAkQCvC7B/d3T56tzBZ7/RuWLrTthqBxg122dJBIgEuBFCcDTA+74Bplqkq+Hv1H5Yn2DGMqmUOO2zhAJEAnwuoSeoX+WQu3x9eQdtdVDxld0rMvWUONWzhAJEAnwdgk9lVR7fJn6R8dxCeuaLajUu8eJBIgEeK+EKepf3yckEK8LdqDYDx8mEiAS4NtKKCqj4CFU+uHDRAJEAlwvYaC2emqQfmtdtkUdP0gkQCTAN5cAtbsu26I/foRIgEiAyySoN74evlHJgX6KvmBavKY/foRIgEiA6yVAGYDpXMMUfc20fmA4/pBIgEiACyRAdT6MMJ1oOkJfOT1VTI/vEwkQCXCxBCAunLqHvI/d09e30+eO7xAJEAlwvYSHbE2xjN1w3cwngavPEAkQCfDdJOxMwZZ+1SQAH7ihpEtPEgkQCXCBBHpT5253l61B6pKmYYG4x3VPEQkQCfDVEtRhte1RN6DAx+6pG6CPxdap40QCRAJcIEF9VsPgmTuU95kV2tUNoGWhjEufIhIgEuBiCWJp/R5Xb0DBH3/8oUpf0aG8S58iEiAS4BoJ1arigozrHkFleRggry2XnicSIBLgAgngmW8qelz3CCo17A7HbxuIBIgEuECC2rOFe061PXhgKbxusHT1GSIBIgG+WgKoz9bvBBUUOjKFXU0KxPbYTDrb2L9kSiRAJMBlErbox4H9Eeqq9SnlBUvwmQNEAkQCfDcJsNO/rnLdPfUKASjjY4+IBIgEuEAC0I86nKKe1XYFPrmiagpl+Ioh6WO7RAJEAnwHCVA9V+CTK1RQsNRxoUxtgY/tEgkQCfBxCWoV1LaKFWzBrsYEFddVlawtn9klEiAS4BoJQD/VqmIFUA2Dqx/h6k5a8cRtkQCRAB+RoD75DoFLX4N7zl4VCRAJcJkEUHtQU/cBX9ddTiRAJMCVEoqaug/A25cTCRAJ8CkJomLlP0UkQCTARyR8NyIBIgEiASIBIgEi4efPn/8DrtY2vxsdGpIAAAAASUVORK5CYII=";
+
+        public const string _base = "iVBORw0KGgoAAAANSUhEUgAAAQQAAAExCAMAAACtYSwmAAAABGdBTUEAALGPC/xhBQAAAwBQTFRFAAAAOzJ0TlJdYGVva3B7IrFM+rI8//IAqNrC2eDi8Pf58fj5AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAxapfrAAAAQB0Uk5T////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////AFP3ByUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAAZdEVYdFNvZnR3YXJlAHBhaW50Lm5ldCA0LjAuMjHxIGmVAAAKEElEQVR4Xu2a7Y7jOBIEb293Z7/e/3nnQsx0HU1RsmS75V4g44dQLBbJqgCmp4GZ//wMPyMBIgEiASIBIgEiASIBIgEiAf4tEn654fVbiQSIBPgXSfi78RUeIgEiAb6nBCbthyWWAdFvvYVIgEiAj0hoIy54PZv6119/rQyB57/RF79OJEAkwKck/LdBoHH4auphqRg8fId230IkQCTAZyWINuUCg5eKIebr4TtI+sbXiASIBPgmEn78+PHPP/8QFJIAFRNocPFGD5EAkQCXSVDnFXv+G2TwoLlEKzclAdiqI1ry1bVPEwkQCXClhN9++601/suff/7JV7MUZAYPop1YkAcyqqdYp1TjZ54iEiAS4DIJQKt4KBWapYfk1IPQKQJXv89DJEAkwJdKaF0bLSVBsNQsPSR3PMByV5safOZlD5EAkQBfLcED334OgNcNlh7jBpl9CaLddKdCGT98kkiASIDLJBRDkqWmECynEshvUQU67rfPEAkQCfClEoCuPG3H0nqXJ5YB0BYeBhV9zYCOQC399mEiASIBPiJhTY0glpHuVUwLvFjBlp8/RiRAJMBXSwBa8qi7tLE2VQxbYpoUbPn5A0QCRAJ8HwmCYvAkDWXA6xVbW+TdwSMiASIBLpAA9OMh71lm28CTdEyTYqvezz8iEiAS4BoJQEue/EZ1zt+A+rWwh12hGujjNdNdkn5+l0iASIDLJAAtef5G3/byS+GuCuHqho6A1y94iASIBLhAQmt/wuABPNUNdu2iMRQLVXox80DGfWwTCRAJcI2E5UfABtPRgLwGF/seHDXWNWTcygaRAJEAF0gA2vDMM9gFd3xjyLA8KAHWZ93HBpEAkQDXSAA6AY89QwU99M+AmpHlloQpQyVL9zEjEiAS4DIJgmZ6PP8Mdml+S4KSx+GgO5gRCRAJcLEEoB/wqLtQVhJg+JnQbw1M85x1BysiASIBrv+Z4AmPUVMPBoQ8gNc3KsOROkXgJlZEAkQCXCmBNjxbxzTZs4zRINY4a9Ye1nCD+1gRCRAJcLGENZr0IJ5nxVoCNztqsHQTMyIBIgGulNDTj7+4uC37/BQKPFjHIEE1JJVn6Vc3iASIBPiIhH7SYephuYXG7NHIwJYCJVn61W0iASIBPithPfI6swWVIANFn1GBn9wlEiAS4FMShOfpmCZ30D0e+4aSfuwAkQCRAB+RIOjTk9yzlZ+i4mXmDj9wmEiASIBPSaBVTbGmtk7VEPjq80QCRAKUBC4RXn8xPKTmp2j3VA2Brz5PJEAkgCRww19//fX6bcep5qfQjFoayvolsailrz5PJEAkwFoCvHLhQar5KTQjhrJ++caeIwEiAaYS4JU7j9CP00MemgDTVw7xete3nyQSIBKglwC+rvH0nUfom++pTnpUzFdBX1YZIPbtJ4kEiATYkQDkn755H671G/eQVycDyi/dNJQEYp+MhAaxbz9JJMAgAXxjx/LqmftVD17PYNe330PefRyjv2f/xR0iASIBjkgQ1OzTblogriNOraiaAfLu4x7ywusbZHwyEhrEvv0kkQCSAP39vvQkS4sdldTlCgqWKhgg7yY6SP7dWO/29xALv3GYSIBIgDdKGODCChTz9WNnJJBhfP3XxMFDf8kQQz0ktJwSCRAJMJUAvu5NcHkF9ZwyPSSFm2hjCv2PLDwQe+9ewpp2bjmoQOj1gUiASICSANT49obvehNcXoHe0lKw7DNagmYHxcOPBQIf2ECnhns0bE8kQCTAjgTwXe+Gh8CLA6helAS+3t6mnfg/kQCRAEckADUav/B174aHHB1g+cN8ozo8eANlPtnOgkftiASIBBgkAGV6pcc3vgmeqO9BPMZNgmZ5eAMFwofbcY3ZEwkQCbCWAFRq9h7f+zJcPgRHoFjQiYJqtcfVN8jU7Ir56mBPJEAkwFQCUKzZ1/iBp+BaR2ckaBBQV9NBBFuFzhLUWXDdPZEAkQBbEkCnNPgaPXEKXehFQxnwehuV6WkCt7iLjvR4Y0UkQCTAjgSh43r9Ie64Q8eFUzNcsUK79Ye6XtGuW3yZSIBIgIcShB4FtTFFDQ9wxNEBfFGHHhVO3R5y9h0qIgEiAQ5KKPzwyoYaK/oMxY4eoauO4APtcnf2ApEAkQBnJRTNxILbuscttiahYgVTfPIY1Otmd/MakQCRAE9I0Os97uyGhgLtevEIHz4MR7jcPb1GJEAkwHMS3McjegNbNiqveh18iCp13G29QCRAJMATEuBItzUdQR+vA6gjCnboiwu39SyRAJEAz0kAP7/dtrcb/bxDzPf333/nqyM6u8Ny7FbMQUHstp4iEiAS4GkJRWtqQU2uYasfto8VCC3BxzagoIp1ULB0Q+eJBIgEeF0C0IBbnNH6dcN9DH0syPjYBusjgry7OU8kQCTAixJ4GtzfNtRUq3z1b4tQmZ6dC9fFPey6rZNEAkQCvC7B/d3T56tzBZ7/RuWLrTthqBxg122dJBIgEuBFCcDTA+74Bplqkq+Hv1H5Yn2DGMqmUOO2zhAJEAnwuoSeoX+WQu3x9eQdtdVDxld0rMvWUONWzhAJEAnwdgk9lVR7fJn6R8dxCeuaLajUu8eJBIgEeK+EKepf3yckEK8LdqDYDx8mEiAS4NtKKCqj4CFU+uHDRAJEAlwvYaC2emqQfmtdtkUdP0gkQCTAN5cAtbsu26I/foRIgEiAyySoN74evlHJgX6KvmBavKY/foRIgEiA6yVAGYDpXMMUfc20fmA4/pBIgEiACyRAdT6MMJ1oOkJfOT1VTI/vEwkQCXCxBCAunLqHvI/d09e30+eO7xAJEAlwvYSHbE2xjN1w3cwngavPEAkQCfDdJOxMwZZ+1SQAH7ihpEtPEgkQCXCBBHpT5253l61B6pKmYYG4x3VPEQkQCfDVEtRhte1RN6DAx+6pG6CPxdap40QCRAJcIEF9VsPgmTuU95kV2tUNoGWhjEufIhIgEuBiCWJp/R5Xb0DBH3/8oUpf0aG8S58iEiAS4BoJ1arigozrHkFleRggry2XnicSIBLgAgngmW8qelz3CCo17A7HbxuIBIgEuECC2rOFe061PXhgKbxusHT1GSIBIgG+WgKoz9bvBBUUOjKFXU0KxPbYTDrb2L9kSiRAJMBlErbox4H9Eeqq9SnlBUvwmQNEAkQCfDcJsNO/rnLdPfUKASjjY4+IBIgEuEAC0I86nKKe1XYFPrmiagpl+Ioh6WO7RAJEAnwHCVA9V+CTK1RQsNRxoUxtgY/tEgkQCfBxCWoV1LaKFWzBrsYEFddVlawtn9klEiAS4BoJQD/VqmIFUA2Dqx/h6k5a8cRtkQCRAB+RoD75DoFLX4N7zl4VCRAJcJkEUHtQU/cBX9ddTiRAJMCVEoqaug/A25cTCRAJ8CkJomLlP0UkQCTARyR8NyIBIgEiASIBIgEi4efPn/8DrtY2vxsdGpIAAAAASUVORK5CYII=";   
         public Color baseprimary = ColorTranslator.FromHtml("#6b707b");
         public Color basesecondary = ColorTranslator.FromHtml("#d9e0e2");
         public Color basehighlight = ColorTranslator.FromHtml("#fab23c");
@@ -40,6 +42,15 @@ namespace primageneratorx
             {
                 doPrima("1x1");
             }
+        }
+
+        private string getName()
+        {
+            WebClient w = new WebClient();
+            byte[] dat = w.DownloadData("https://namey.muffinlabs.com/name.json?type=surname");
+            string name = System.Text.Encoding.UTF8.GetString(dat);
+            name = name.Substring(2, name.Length - 4);
+            return name;
         }
 
         public void doPrima(string dim)
@@ -68,6 +79,22 @@ namespace primageneratorx
             foreach (Point p in positions)
             {
                 Bitmap prima = GetNewPrima(tag);
+                if (checkBox2.Checked)
+                {
+                    using (Graphics graphics = Graphics.FromImage(prima))
+                    {
+                        string primaName = getName();
+                        PointF point = new PointF(2f, 280f);
+                        Font lucFont = new Font("Lucida Console", 14);
+                        SizeF size = graphics.MeasureString(primaName, lucFont);
+                        RectangleF rect = new RectangleF(point, size);
+                        rect.Height = rect.Height + 1;
+                        graphics.FillRectangle(Brushes.Black, rect);
+                        point.Y = point.Y + 2;
+                        graphics.DrawString(primaName, lucFont, Brushes.White, point);
+                        graphics.Dispose();
+                    }
+                }
                 gr.DrawImage(prima, p);
                 count++;
                 this.Invoke(new MethodInvoker(delegate ()
@@ -137,7 +164,8 @@ namespace primageneratorx
             Color normalface = Color.FromArgb(78, 82, 93);
 
             Random rnd = new Random();
-            Bitmap primabitmap;          
+            Bitmap primabitmap;
+
             byte[] byteBuffer = Convert.FromBase64String(_base);
             MemoryStream memoryStream = new MemoryStream(byteBuffer);
             memoryStream.Position = 0;
@@ -192,47 +220,19 @@ namespace primageneratorx
                     }
                     else if (p == basefeet)
                     {
-                        if (tag == "-c")
-                        {
-                            drawing.FillRectangle(fe, i, o, 1, 1);
-                        }
-                        else
-                        {
-                            drawing.FillRectangle(len, i, o, 1, 1);
-                        }
+                        drawing.FillRectangle(fe, i, o, 1, 1);
                     }
                     else if (p == baselegs)
                     {
-                        if (tag == "-c")
-                        {
-                            drawing.FillRectangle(le, i, o, 1, 1);
-                        }
-                        else
-                        {
-                            drawing.FillRectangle(len, i, o, 1, 1);
-                        }
+                        drawing.FillRectangle(le, i, o, 1, 1);
                     }
                     else if (p == baseface)
                     {
-                        if (tag == "-c")
-                        {
-                            drawing.FillRectangle(fa, i, o, 1, 1);
-                        }
-                        else
-                        {
-                            drawing.FillRectangle(fan, i, o, 1, 1);
-                        }
+                         drawing.FillRectangle(fan, i, o, 1, 1);
                     }
                     else if (p == baseweird)
                     {
-                        if (tag == "-c")
-                        {
-                            drawing.FillRectangle(we, i, o, 1, 1);
-                        }
-                        else
-                        {
-                            drawing.FillRectangle(wen, i, o, 1, 1);
-                        }
+                        drawing.FillRectangle(we, i, o, 1, 1);
                     }
                     else
                     {
